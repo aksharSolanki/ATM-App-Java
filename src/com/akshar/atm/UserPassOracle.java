1bd9204fddf6ae7093af64dda36bb0c6b07bf655
Akshar <akshar.bit@gmail.com>
2021-08-30 00:13:28 +0530

    Added cheatsheet

--- src/com/akshar/atm/UserPassOracle.java
+++ src/com/akshar/atm/UserPassOracle.java
@@ -3,6 +3,6 @@ package com.akshar.atm;
 
 public class UserPassOracle {
     
-    public final static String USERNAME = "n01418888";
+    public final static String USERNAME = "username";
     public final static String PASSWORD = "oracle";
 }
